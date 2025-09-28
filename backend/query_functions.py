c30fffe7bdb5387e61580e13b1412ba338ca29a9
Walkerovski <barbardian@wp.pl>
2025-09-28 04:51:36 -0400

    lerner types

--- backend/query_functions.py
+++ backend/query_functions.py
@@ -51,3 +51,77 @@ def find_peers_with_textbooks(neodriver, student_name: str, course_id: str, min_
     database_=neodriver._db
     )
     return records
+
+def textbooks_popularity_among_courses_groupped_by_grades(neodriver, course_id: str):
+    records, summary, keys = neodriver._driver.execute_query("""
+    // Find how many students (per grade) interacted with each textbook
+    MATCH (c:Course {id: $course_id})<- [g:COMPLETED]-(s:Student)-[:INTERACTED_WITH]->(t:Textbook)<-[:ASSIGNED_TO_A_COURSE]-(c)
+    WITH t, g.grade AS grade, count(DISTINCT s) AS readers
+    // Count total students with that grade in the course
+    MATCH (c:Course {id: $course_id})<- [g2:COMPLETED]-(s2:Student)
+    WITH t, grade, readers, g2.grade AS grade2, count(DISTINCT s2) AS total_students
+    WHERE grade = grade2
+    RETURN t.id AS textbook_id,
+           t.name AS textbook_name,
+           grade,
+           readers,
+           total_students,
+           (toFloat(readers) / total_students) AS proportion
+    ORDER BY grade DESC, readers DESC
+    """,
+    course_id=course_id,
+    database_=neodriver._db
+    )
+    return records
+
+def learner_types_enrolled_in_a_course(neodriver, course_id: str):
+    records, summary, keys = neodriver._driver.execute_query("""
+    MATCH (c:Course {id: $course_id})<- [g:COMPLETED]-(s:Student)
+    WITH c, s.learningStyle AS learning_style, g.grade AS grade, count(DISTINCT s) AS students
+    RETURN c.id AS c_id,
+           c.name AS c_name,
+           grade,
+            learning_style,
+           students
+    ORDER BY grade DESC, learning_style DESC
+    """,
+    course_id=course_id,
+    database_=neodriver._db
+    )
+    return records
+
+def find_student_id(neodriver, student_name: str):
+    records, summary, keys = neodriver._driver.execute_query("""
+    MATCH (me:Student {name: $student_name})
+    RETURN me.id as id
+    """,
+    student_name=student_name,
+    database_=neodriver._db
+    )
+    return records[0]["id"]
+
+def find_student_degree(neodriver, student_id: str):
+    records, summary, keys = neodriver._driver.execute_query("""
+    // Find the degree(s) of the given student
+    MATCH (me:Student {id: $student_id})-[:DEGREE]->(d:Degree)
+    RETURN d.name AS degree_name
+    """,
+    student_id=student_id,
+    database_=neodriver._db
+    )
+    return records[0]["degree_name"]
+
+def find_alumni_that_finished_from_same_degree(neodriver, degree_id: str):
+    records, summary, keys = neodriver._driver.execute_query("""
+    MATCH (alumni:Student)-[:DEGREE]->(d:Degree {degreeId: $degree_id})
+    WHERE alumni.expectedGraduation < date()
+    RETURN alumni.studentId AS student_id,
+           alumni.name AS student_name,
+           d.name AS degree_name,
+           alumni.expectedGraduation AS graduation_date
+    ORDER BY alumni.expectedGraduation
+    """,
+    degree_id=degree_id,
+    database_=neodriver._db
+    )
+    return [dict(record) for record in records]
