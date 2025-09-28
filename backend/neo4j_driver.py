db3a17639d8b519de9ae2e375e669023f8b7d278
VihaanMotwani <vihaanmotwani16@gmail.com>
2025-09-27 20:45:24 -0400

    added run_query

--- backend/neo4j_driver.py
+++ backend/neo4j_driver.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 import os
 
 class Neo4jDriver:
-    def __init__(self, uri=None, user=None, password=None):
+    def __init__(self, uri=None, user=None, password=None, database=None):
         self._uri = uri or os.getenv("NEO4J_DATABASE_URI")
         self._user = user or os.getenv("NEO4J_USERNAME")
         self._pwd = password or os.getenv("NEO4J_PASSWORD")
+        self._db = database or os.getenv("NEO4J_DATABASE")
         self._driver = None
 
     def connect(self):
@@ -21,3 +22,16 @@ class Neo4jDriver:
     def close(self):
         if self._driver:
             self._driver.close()
+
+    def run_query(self, query: str, database: str = None):
+        if not self._driver:
+            raise RuntimeError("Driver is not connected. Call connect() first.")
+        
+        db = database or self._db
+
+        records, summary, keys = self._driver.execute_query(
+            query,
+            database_=db
+        )
+        
+        return records, summary, keys
